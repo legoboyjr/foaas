f903faa4ab4d94a62d92d7831297bffc8fac45f6
Alexander Thompson <legoboyjr@gmail.com>
2018-11-16 19:29:45 -0500

    Updates

---
+++ precache-manifest.0539b0b12c8b7d626a08b3a4017743cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b825db6b00af3af78858b849e3e0e21f",
+    "url": "/foaas/static/media/Cartoon.b825db6b.jpg"
+  },
+  {
+    "revision": "c37986bb37c66e48d3f6",
+    "url": "/foaas/static/js/runtime~main.c37986bb.js"
+  },
+  {
+    "revision": "d46db90a09006289771c",
+    "url": "/foaas/static/js/main.d46db90a.chunk.js"
+  },
+  {
+    "revision": "b9188acb0ba309136330",
+    "url": "/foaas/static/js/1.b9188acb.chunk.js"
+  },
+  {
+    "revision": "d46db90a09006289771c",
+    "url": "/foaas/static/css/main.464524e7.chunk.css"
+  },
+  {
+    "revision": "b9188acb0ba309136330",
+    "url": "/foaas/static/css/1.8066da10.chunk.css"
+  },
+  {
+    "revision": "d9685663fdd495b1bf4a725f985f20c8",
+    "url": "/foaas/index.html"
+  }
+];
